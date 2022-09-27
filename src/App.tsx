3133aeb8a1f4b08ae960c0a1721dd2b63e020277
ynjo-world <helloworld2126@gmail.com>
2022-09-27 11:01:46 +0900

    initial setting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+    </div>
+  );
+}
+
+export default App;
