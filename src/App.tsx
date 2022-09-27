3f2fbea674b00ee7fedad91236e486b2b171c12e
ynjo-world <helloworld2126@gmail.com>
2022-09-27 16:30:33 +0900

    공통 레이아웃 컴포넌트 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 import Button from "@mui/material/Button";
@@ -7,8 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
